775c43f71af498daeb17792489e751d18897a224
gao,xuefeng <59185890+45717335@users.noreply.github.com>
2020-06-29 13:15:31 +0800

    50to70

--- main.ts
+++ main.ts
@@ -1,4 +1,28 @@
+function ShowVoice (数字: number) {
+    basic.clearScreen()
+    if (数字 <= 50) {
+        led.plot(2, 2)
+    } else if (数字 < 55) {
+        led.plot(2, 2)
+        led.plot(2, 3)
+    } else if (数字 <= 60) {
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(3, 3)
+    } else if (数字 <= 65) {
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(3, 3)
+        led.plot(3, 2)
+    } else if (数字 <= 70) {
+        led.plot(2, 2)
+        led.plot(2, 3)
+        led.plot(3, 3)
+        led.plot(3, 2)
+        led.plot(3, 1)
+    }
+}
 startbit.startbit_Init()
 basic.forever(function () {
-	
+    ShowVoice(startbit.startbit_getSoundVolume())
 })
