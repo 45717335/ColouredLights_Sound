bb8cafcc47e5c04aca91d738ae6e82d4b3dfb1c0
gao,xuefeng <59185890+45717335@users.noreply.github.com>
2020-07-03 17:13:20 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -74,9 +74,79 @@ function ShowVoice (数字: number) {
         basic.pause(100)
     }
 }
+function ShowVoice2 (数字: number, 数字2: number) {
+    // 新声音小于旧声音，需要降低音量
+    if (数字 < 数字2) {
+        if (数字 < 95 && 数字2 >= 95) {
+            led.unplot(1, 4)
+        }
+        if (数字 < 90 && 数字2 >= 90) {
+            led.unplot(1, 3)
+        }
+        if (数字 < 85 && 数字2 >= 85) {
+            led.unplot(1, 2)
+        }
+        if (数字 < 80 && 数字2 >= 80) {
+            led.unplot(1, 1)
+        }
+        if (数字 < 75 && 数字2 >= 75) {
+            led.unplot(2, 1)
+        }
+        if (数字 < 70 && 数字2 >= 70) {
+            led.unplot(3, 1)
+        }
+        if (数字 < 65 && 数字2 >= 65) {
+            led.unplot(3, 2)
+        }
+        if (数字 < 60 && 数字2 >= 60) {
+            led.unplot(3, 3)
+        }
+        if (数字 < 55 && 数字2 >= 55) {
+            led.unplot(2, 3)
+        }
+        if (数字 < 50 && 数字2 >= 50) {
+            led.unplot(2, 2)
+        }
+    }
+    // 新声音小于旧声音，需要降低音量
+    if (数字 > 数字2) {
+        if (数字2 < 95 && 数字 >= 95) {
+            led.plot(1, 4)
+        }
+        if (数字2 < 90 && 数字2 >= 90) {
+            led.plot(1, 3)
+        }
+        if (数字2 < 85 && 数字 >= 85) {
+            led.plot(1, 2)
+        }
+        if (数字2 < 80 && 数字 >= 80) {
+            led.plot(1, 1)
+        }
+        if (数字2 < 75 && 数字 >= 75) {
+            led.plot(2, 1)
+        }
+        if (数字2 < 70 && 数字 >= 70) {
+            led.plot(3, 1)
+        }
+        if (数字2 < 65 && 数字 >= 65) {
+            led.plot(3, 2)
+        }
+        if (数字2 < 60 && 数字 >= 60) {
+            led.plot(3, 3)
+        }
+        if (数字2 < 55 && 数字 >= 55) {
+            led.plot(2, 3)
+        }
+        if (数字2 < 50 && 数字 >= 50) {
+            led.plot(2, 2)
+        }
+    }
+}
+let v2 = 0
+let v1 = 0
 startbit.startbit_Init()
-let i1 = 40
 basic.forever(function () {
-    ShowVoice(startbit.startbit_getSoundVolume())
-    basic.pause(100)
+    v1 = v2
+    v2 = startbit.startbit_getSoundVolume()
+    ShowVoice2(v2, v1)
 })
